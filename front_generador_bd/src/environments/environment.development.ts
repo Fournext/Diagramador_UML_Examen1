cd0360116d8c2728d98e3f6cd696b3bfa0d847df
Fournext <sebastianzc04@gmail.com>
2025-09-24 16:39:01 -0400

    feat: despliegue a produccion 1

--- front_generador_bd/src/environments/environment.development.ts
+++ front_generador_bd/src/environments/environment.development.ts
@@ -4,6 +4,8 @@ export const environment = {
     production: false,
     wsPort,                // 👈 puerto configurable
     wsPath: '/ws/canvas/',        // 👈 path base
-    endpoint_python: `http://127.0.0.1:${wsPort}/`,
-    endpoint_java: `http://127.0.0.1:${portJava}/`
+    //endpoint_python: `http://127.0.0.1:${wsPort}/`,
+    endpoint_python: `https://ec2-34-228-19-253.compute-1.amazonaws.com:${wsPort}/`,
+    //endpoint_java: `http://127.0.0.1:${portJava}/`
+    endpoint_java: `https://ec2-34-228-19-253.compute-1.amazonaws.com:${portJava}/`
 };
