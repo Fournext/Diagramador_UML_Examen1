4058e8fdcfb53e37153a1b0e2ccb18f96f97931e
Fournext <sebastianzc04@gmail.com>
2025-09-24 20:17:27 -0400

    fix: vuelta a aws nativo

--- front_generador_bd/src/environments/environment.development.ts
+++ front_generador_bd/src/environments/environment.development.ts
@@ -5,9 +5,7 @@ export const environment = {
     wsPort,                // 👈 puerto configurable
     wsPath: '/ws/canvas/',        // 👈 path base
     //endpoint_python: `http://127.0.0.1:${wsPort}/`,
-    //endpoint_python: `https://ec2-34-228-19-253.compute-1.amazonaws.com:${wsPort}/`,
-    endpoint_python: `https://django.sw1.fournext.me/`,
+    endpoint_python: `https://ec2-34-228-19-253.compute-1.amazonaws.com:${wsPort}/`,
     //endpoint_java: `http://127.0.0.1:${portJava}/`
-    //endpoint_java: `https://ec2-34-228-19-253.compute-1.amazonaws.com:${portJava}/`
-    endpoint_java: `https://spring.sw1.fournext.me/`
+    endpoint_java: `https://ec2-34-228-19-253.compute-1.amazonaws.com:${portJava}/`
 };
